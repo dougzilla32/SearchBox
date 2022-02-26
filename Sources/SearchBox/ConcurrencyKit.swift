7cdbbc891aecd671b2c913f10724d41d4e9b61df
Doug Stein <dougstein@gmail.com>
2022-02-25 21:58:56 -0800

    Backwards compatible URLSession.data(from: URL) function

--- Sources/SearchBox/ConcurrencyKit.swift
+++ Sources/SearchBox/ConcurrencyKit.swift
@@ -114,3 +114,22 @@ extension ThrowingTaskGroup {
         return addTask(priority: priority, operation: operation)
     }
 }
+
+///  Taken from https://www.swiftbysundell.com/articles/making-async-system-apis-backward-compatible/
+@available(macOS, deprecated: 12, message: "Use the built-in API instead")
+extension URLSession {
+    func data(from url: URL) async throws -> (Data, URLResponse) {
+        try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: url) { data, response, error in
+                guard let data = data, let response = response else {
+                    let error = error ?? URLError(.badServerResponse)
+                    return continuation.resume(throwing: error)
+                }
+
+                continuation.resume(returning: (data, response))
+            }
+
+            task.resume()
+        }
+    }
+}
